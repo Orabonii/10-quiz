fe163e6def4131b19781ad89310efa7ccbb2023e
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 18:12:18 -0300

    Add files via upload

---
+++ projeto10/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import Quiz from './/Components/Projeto10/Projeto10'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <Quiz/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
